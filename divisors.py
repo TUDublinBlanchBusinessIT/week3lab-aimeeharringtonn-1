6065d2d7e01be7c60a61b091277c2010b6034d3c
aimeeharringtonn <151195843+aimeeharringtonn@users.noreply.github.com>
2024-02-08 03:50:08 +0000

    Update divisors.py

--- divisors.py
+++ divisors.py
@@ -3,8 +3,8 @@
 #divisors.py
 
 
-def divisors(num)
-divisors_list=[]
+def divisors(num):
+    divisors_list=[]
     for i in range(1, num + 1):
         if num % i == 0:
             divisors_list.append(i)
